00336a7da67b294d06a530b6da0d6527ab8885d5
kdgbunny <kdgbunny@gmail.com>
2016-05-28 13:49:02 +0900

    김동건

--- app/src/main/java/c/m/mobile8/MemoListActivity.java
+++ app/src/main/java/c/m/mobile8/MemoListActivity.java
@@ -9,9 +9,9 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -60,6 +60,7 @@ public class MemoListActivity extends AppCompatActivity {
             }
         });
 
+
     }
 
     private class MemoListViewAdapter extends BaseAdapter {
@@ -99,6 +100,27 @@ public class MemoListActivity extends AppCompatActivity {
         public TextView textViewMemoTitle;
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_memo_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     //implements Back key pressed
     @Override
