14e95a5d68891809590f1705cf32bc50be8cc532
kdgbunny <kdgbunny@gmail.com>
2016-05-29 06:50:42 +0900

    김동건

--- app/src/main/java/c/m/mobile8/fragments/MemoListFragment.java
+++ app/src/main/java/c/m/mobile8/fragments/MemoListFragment.java
@@ -1,5 +1,6 @@
 package c.m.mobile8.fragments;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,7 +10,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import java.util.List;
@@ -29,9 +33,13 @@ import c.m.mobile8.utils.DBManager;
 public class MemoListFragment extends Fragment {
     private final String TAG = "MemoListFragment";
 
-    private ListView listViewMemoList;
+    private ListView listViewMemoList, listVIewSearchList;
     private MemoListViewAdapter memoListViewAdapter;
-    private TextView textViewNoMemo;
+    private TextView textViewNoMemo, textViewNoSearch;
+    private EditText editTextSearch;
+    private ImageView imageViewSearch;
+    private boolean isSearchMode = false;
+    List<Memo> searchMemoList;
 
     public MemoListFragment() {
         // Required empty public constructor
@@ -42,7 +50,39 @@ public class MemoListFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_memo_list, container, false);
         textViewNoMemo = (TextView)rootView.findViewById(R.id.textViewNoMemo);
+        textViewNoSearch = (TextView)rootView.findViewById(R.id.textViewNoMemo);
+        editTextSearch = (EditText)rootView.findViewById(R.id.editTextSearch);
         listViewMemoList = (ListView)rootView.findViewById(R.id.listViewMemoList);
+        listVIewSearchList = (ListView)rootView.findViewById(R.id.listVIewSearchList);
+
+        editTextSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, "edit click");
+            }
+        });
+        imageViewSearch = (ImageView)rootView.findViewById(R.id.imageViewSearch);
+        imageViewSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextSearch.clearFocus();
+                setKeyboard(false);
+                if(!editTextSearch.getText().toString().equals("")) {
+                    isSearchMode = true;
+                    searchMemoList = DBManager.getInstance(getActivity()).searchMemo(editTextSearch.getText().toString());
+                    if(searchMemoList.size() == 0) textViewNoSearch.setText("일치하는 결과가 없습니다.");
+                    else textViewNoSearch.setText("");
+                    listViewMemoList.setVisibility(View.GONE);
+                    listVIewSearchList.setVisibility(View.VISIBLE);
+                    listVIewSearchList.setAdapter(new MemoListViewAdapter(getActivity().getApplicationContext(), searchMemoList));
+                } else {
+                    textViewNoSearch.setText("");
+                    listViewMemoList.setVisibility(View.VISIBLE);
+                    listVIewSearchList.setVisibility(View.GONE);
+                }
+            }
+        });
+
 
         List<Memo> memoList = ((MainActivity)getActivity()).mMemoList;
         reloadData();
@@ -53,60 +93,82 @@ public class MemoListFragment extends Fragment {
         }
         listViewMemoList.setAdapter(memoListViewAdapter);
 
-        listViewMemoList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                List<Memo> memoList = ((MainActivity)getActivity()).mMemoList;
-                enterDetailView(memoList.get(position).getId());
-            }
-        });
-        listViewMemoList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
-                final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
-                alertDialogBuilder
-                        .setTitle("삭제")
-                        .setPositiveButton("확인",new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                List<Memo> memoList = ((MainActivity)getActivity()).mMemoList;
-                                DBManager.getInstance(getActivity().getApplicationContext()).deleteMemo(memoList.get(position).getId());
-                                ((MainActivity)getActivity()).reloadData();
-                            }
-                        })
+        listViewMemoList.setOnItemClickListener(itemClickListener);
+        listViewMemoList.setOnItemLongClickListener(itemLongClickListener);
+        listVIewSearchList.setOnItemClickListener(itemClickListener);
+        listVIewSearchList.setOnItemLongClickListener(itemLongClickListener);
+
+        return rootView;
+    }
+
+    ListView.OnItemClickListener itemClickListener = new ListView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            List<Memo> memoList;
+            if(!isSearchMode) memoList = ((MainActivity)getActivity()).mMemoList;
+            else memoList = searchMemoList;
+            enterDetailView(memoList.get(position).getId());
+        }
+    };
+    ListView.OnItemLongClickListener itemLongClickListener = new ListView.OnItemLongClickListener() {
+        @Override
+        public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+            final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+            alertDialogBuilder
+                    .setTitle("삭제")
+                    .setPositiveButton("확인",new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            List<Memo> memoList = ((MainActivity)getActivity()).mMemoList;
+                            DBManager.getInstance(getActivity().getApplicationContext()).deleteMemo(memoList.get(position).getId());
+                            ((MainActivity)getActivity()).reloadData();
+                        }
+                    })
                     .setNegativeButton("취소", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             dialog.cancel();
                         }
                     });
-                AlertDialog alertDialog = alertDialogBuilder.create();
-                alertDialog.show();
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
 
-                return true;
-            }
-        });
-        return rootView;
-    }
+            return true;
+        }
+    };
 
     public void reloadData() {
         List<Memo> memoList = ((MainActivity)getActivity()).mMemoList;
         setTextNoMemo(memoList.size());
+        isSearchMode = false;
+
         if(memoListViewAdapter != null) {
             memoListViewAdapter.setMemoList(memoList);
             memoListViewAdapter.notifyDataSetChanged();
         }
+        if(listVIewSearchList != null) listVIewSearchList.setVisibility(View.GONE);
+    }
+
+    private void setKeyboard(boolean toggle) {
+        InputMethodManager keyboard = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        keyboard.hideSoftInputFromWindow(editTextSearch.getWindowToken(), 0);
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        editTextSearch.setText("");
+        reloadData();
     }
 
     private void setTextNoMemo(int size) {
         if(size == 0) {
             textViewNoMemo.setText("표시할 메모가 없습니다.");
+            editTextSearch.setVisibility(View.INVISIBLE);
+            listViewMemoList.setVisibility(View.INVISIBLE);
         } else {
             textViewNoMemo.setText("");
+            editTextSearch.setVisibility(View.VISIBLE);
+            listViewMemoList.setVisibility(View.VISIBLE);
         }
     }
 
