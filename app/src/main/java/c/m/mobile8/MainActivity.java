5494040da533b641a6b06b5df4ff43fc336861f8
kdgbunny <kdgbunny@gmail.com>
2016-05-28 10:57:11 +0900

    test

--- app/src/main/java/c/m/mobile8/MainActivity.java
+++ app/src/main/java/c/m/mobile8/MainActivity.java
@@ -3,6 +3,7 @@ package c.m.mobile8;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e("TEST", "test");
     }
 }
